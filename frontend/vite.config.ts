c1451ffe7a636d9d6c4358b36b468d4c4c69de56
Konstantinos Athanasiou <dinosath0@gmail.com>
2025-06-29 22:14:05 +0300

    add frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tanstackStart({}),
+    tailwindcss(),
+  ],
+})
